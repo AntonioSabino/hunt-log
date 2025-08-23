73a069994221cf23b5c382b85a8a0a90357f9fd2
Antonio Sabino <aslimajr@outlook.com>
2025-08-23 18:53:33 -0300

    criando weapon repository

---
+++ src/weapons/repositories/weapons.repository.ts
@@ -0,0 +1,22 @@
+import { Weapon } from '../entities/weapon.entity';
+
+export type FindWeaponsParams = {
+  type?: Weapon['type'];
+  q?: string;
+};
+
+export type CreateWeaponData = Omit<Weapon, 'id'>;
+export type UpdateWeaponData = Partial<Omit<Weapon, 'id'>>;
+
+export abstract class WeaponsRepository {
+  abstract create(data: CreateWeaponData): Promise<Weapon>;
+  abstract findAll(filter: FindWeaponsParams): Promise<Weapon[]>;
+  abstract findById(id: Weapon['id']): Promise<Weapon | null>;
+  abstract update(
+    id: Weapon['id'],
+    data: UpdateWeaponData,
+  ): Promise<Weapon | null>;
+  abstract delete(id: Weapon['id']): Promise<boolean>;
+}
+
+export const WEAPONS_REPOSITORY = Symbol('WEAPONS_REPOSITORY');
