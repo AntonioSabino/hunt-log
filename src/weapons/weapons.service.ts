797922d1e3889b177ee1fc318a44b1686a644c02
Antonio Sabino <aslimajr@outlook.com>
2025-08-23 18:55:37 -0300

    adicionando weapon service

---
+++ src/weapons/weapons.service.ts
@@ -0,0 +1,50 @@
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateWeaponDto } from './dto/create-weapon.dto';
+import { UpdateWeaponDto } from './dto/update-weapon.dto';
+import {
+  FindWeaponsParams,
+  WEAPONS_REPOSITORY,
+  WeaponsRepository,
+} from './repositories/weapons.repository';
+
+@Injectable()
+export class WeaponsService {
+  constructor(
+    @Inject(WEAPONS_REPOSITORY)
+    private readonly repo: WeaponsRepository,
+  ) {}
+
+  create(dto: CreateWeaponDto) {
+    return this.repo.create(dto);
+  }
+
+  findAll(params: FindWeaponsParams) {
+    return this.repo.findAll(params);
+  }
+
+  async findOne(id: string) {
+    const weapon = await this.repo.findById(id);
+
+    if (!weapon) {
+      throw new NotFoundException('Weapon not found');
+    }
+
+    return weapon;
+  }
+
+  async update(id: string, dto: UpdateWeaponDto) {
+    const update = await this.repo.update(id, dto);
+    if (!update) {
+      throw new NotFoundException('Weapon not found');
+    }
+    return update;
+  }
+
+  async remove(id: string) {
+    const ok = await this.repo.delete(id);
+    if (!ok) {
+      throw new NotFoundException('Weapon not found');
+    }
+    return ok;
+  }
+}
